a7baa103257effb983ede610bd9395f0bcfdd2cc
ErickVixtha <145307134+ErickVixtha@users.noreply.github.com>
2023-10-03 02:48:18 -0600

    Add files via upload

---
+++ Practica 3.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val x1 = 4.0
+    val y1 = 3.0
+    val x2 = -3.0
+    val y2 = -2.0
+
+    val pendiente = (y2 - y1) / (x2 - x1)
+
+    println("La pendiente entre los puntos P1($x1, $y1) y P2($x2, $y2) es igual a $pendiente")
+}
